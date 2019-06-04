53a01f41e7d3a519a162513ceaf9c173695dc386
choiseungwon <b088081@gmail.com>
2019-06-04 22:41:35 +0900

    commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,7 +26,7 @@ module.exports = {
                 collapsable: true,
                 children: [
                     'markdown',
-                    'Spring-Batch'
+                    'SpringBatch'
                 ]
             }]
         }
