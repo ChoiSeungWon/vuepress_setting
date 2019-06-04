7cd0368d17d3c3e329890f083fc5223fba683d30
choiseungwon <b088081@gmail.com>
2019-06-04 21:27:37 +0900

    swchoi menu add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,34 @@
 
 module.exports = {
+    home : true,
     title : 'Hello VuePress',
     description : 'Just playing around',
-    base : 'vuepress-setting'
+    base : '/vuepress_setting/',
+    themeConfig: {
+        repo: 'ChoiSeungWon/vuepress_setting',
+        editLinks: true,
+        docsDir: 'docs',
+        lastUpdated: 'Last Updated',
+        search: true,
+        sidebarDepth: 3,
+        nav: [{
+            text: 'Home',
+            link: '/'
+        },
+            {
+                text: '최승원',
+                link: '/swchoi/'
+            }
+        ],
+        sidebar: {
+            '/swchoi/': [{
+                title: 'DOCUMENT',
+                collapsable: true,
+                children: [
+                    'markdown',
+                    'Spring-Batch'
+                ]
+            }]
+        }
+    }
 }
